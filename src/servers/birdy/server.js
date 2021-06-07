386e1ea86e68c93d49633304246034f8e45c7dea
Srettha Terananont <strayedpeople@gmail.com>
2021-06-07 23:39:10 +0700

    feat(server): add sing endpoint for bird

--- src/servers/birdy/server.js
+++ src/servers/birdy/server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const http = require('http')
 const HttpStatus = require('http-status')
 
+const { birdQueue } = require('../../worker')
 const { getAllBirds } = require('./service')
 
 /**
@@ -31,6 +32,29 @@ function createHttpServer(tracer) {
         span.setStatus({ code: api.SpanStatusCode.OK })
 
         res.status(HttpStatus.OK).json({ birds })
+      } catch (err) {
+        span.setStatus({ code: api.SpanStatusCode.ERROR, message: err.message })
+        res.status(HttpStatus.BAD_REQUEST).send({ message: err.message })
+      }
+      span.end()
+    })
+  })
+
+  app.post('birds/:birdId/sing', (req, res) => {
+    const { birdId } = req.params
+
+    const currentSpan = api.getSpan(api.context.active())
+    console.log(`traceid: ${currentSpan.context().traceId}`)
+    const span = tracer.startSpan(`${birdQueue.name}`, {
+      kind: api.SpanKind.PRODUCER,
+    })
+
+    api.context.with(api.setSpan(api.ROOT_CONTEXT, span), async () => {
+      try {
+        await birdQueue.add('bird-sings-a-song', { birdId, tracer })
+        span.setStatus({ code: api.SpanStatusCode.OK })
+
+        res.status(HttpStatus.CREATED).send()
       } catch (err) {
         span.setStatus({ code: api.SpanStatusCode.ERROR, message: err.message })
         res.status(HttpStatus.BAD_REQUEST).send({ message: err.message })
